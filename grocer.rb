95139538edb8e79eacdb901a7544bf5403b527fe
Matthew Borkowski <barnabytheplumber@gmail.com>
2019-10-04 22:26:42 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -48,6 +48,4 @@ def checkout(array, coupons)
   applied_discount = apply_clearance(apply_coupons)
   total = applied_discount.reduce(0) { |acc, key, value)| acc += value[:price] * value[:count]}
   total > 100 ? total * 0.9 : total
-  
-  return total
 end
