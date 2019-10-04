1b5fe125d6bd0d784ba153e8b73dea82041286f1
Matthew Borkowski <barnabytheplumber@gmail.com>
2019-10-04 22:31:52 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -24,10 +24,11 @@ def apply_coupons(cart, coupons)
       if cart.has_key?(item)
         if cart[item][:count] >= coupon[:num] && !cart.has_key?("#{item} W/COUPON")
           cart["#{item} W/COUPON"] = {price: coupon[:cost] / coupon[:num], clearance: cart[item][:clearance], count: coupon[:num]}
+          cart[item][:count] -= coupon[:num]
         elsif cart[item][:count] >= coupon[:num] && cart.has_key?("#{item} W/COUPON")
           cart["#{item} W/COUPON"][:count] += coupon[:num]
+          cart[item][:count] -= coupon[:num]
         end
-        cart[item][:count] -= coupon[:num]
       end
     end
   
@@ -35,9 +36,17 @@ def apply_coupons(cart, coupons)
 end
 
 def apply_clearance(cart)
+  cart.each do |product_name, stats|
+    stats[:price] -= stats[:price] * 0.2 if stats[:clearance] 
+  end
   
+  return cart
 end
 
-def checkout(cart, coupons)
-  # code here
+def checkout(array, coupons)
+  hash_cart = consolidate_cart(array)
+  applied_coupons = apply_coupons(hash_cart, coupons)
+  applied_discount = apply_clearance(applied_coupons)
+  total = applied_discount.reduce(0) { |acc, (key, value)| acc += value[:price] * value[:count]}
+  total > 100 ? total * 0.9 : total
 end
